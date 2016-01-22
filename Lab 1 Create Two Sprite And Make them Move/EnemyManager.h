b7f65cf2e627fb5a00202e243a342d198c078b03
DaveMold <dave.mdaz@yahoo.com>
2016-01-22 15:55:38 +0000

    more progress on doxygen.

--- Lab 1 Create Two Sprite And Make them Move/EnemyManager.h
+++ Lab 1 Create Two Sprite And Make them Move/EnemyManager.h
@@ -10,26 +10,26 @@ private:
     vector<Swarmer*> enemies;
     vector<Factory *> factories;
     vector<Predator*> preds;
-	int swarmingRange; //when the swarming enemies are with in this range they will stop flocking to swarm on the player.
+	int swarmingRange; //!<when the swarming enemies are with in this range they will stop flocking to swarm on the player.
 	enum swarmAIType { SWARM, FLOCKING };
 	swarmAIType swarmAI;
 
 
 public:
 	
-	EnemyManager();
+	EnemyManager();//!< constructor
 	~EnemyManager();
-	void Draw(sf::RenderWindow &w, sf::Vector2f &wb);
+	void Draw(sf::RenderWindow &w, sf::Vector2f &wb);//!<  Draws the enemies.
 	int getSize();
 	Swarmer getEnemy(int i);
-	void addEnemy(Swarmer* b);
-    void addFactory(Factory* fac);
-	void flocking();
+	void addEnemy(Swarmer* b);//!< adds a passed enemy to a list.
+    void addFactory(Factory* fac); //!< adds the factory to a list.
+	void flocking(); //!< applies flocking to the enemies.
 	void CheckCollisions();
 	void swarming(Pvector mousepos);
     void UpdateFactories(Player* p);
 	void swarmEAI(Pvector target);
-    void addPredator(float x, float y);
-    void UpdatePredators(Pvector playerPos);
+    void addPredator(float x, float y); //!<  create a predidtor.
+    void UpdatePredators(Pvector playerPos); //!<  updates preditors
 };
 #endif
--- Lab 1 Create Two Sprite And Make them Move/Player.h
+++ Lab 1 Create Two Sprite And Make them Move/Player.h
@@ -14,18 +14,18 @@ class Player {
 	sf::View followPlayer, miniMapView;
     Pvector* velocity;
 public:
-	Player();
-	void Move();
-	sf::Vector2f ScreenRap(sf::Vector2f pos, sf::Vector2f &w);
-	void Update(sf::Vector2f &w);
+	Player();//!< constructor.
+	void Move();//!< updates the position with the values from the other methods
+	sf::Vector2f ScreenRap(sf::Vector2f pos, sf::Vector2f &w); //!< basic screen rap on the large area.
+	void Update(sf::Vector2f &w); //!< handles the keyboard input and what should happen in each circumstance as well as update the views for the viewport and minimap.
 	void SetPos(sf::Vector2f pos);
 	sf::Vector2f GetPos();
 	sf::View getView();
 	sf::View getMiniMapView();
-	void SpeedUp();
-	void SlowDown();
-	void Draw(sf::RenderWindow &w);
-	sf::FloatRect GetBounds();
-    Pvector* getVelocity() { return velocity; }
+	void SpeedUp();//!< increase the speed vairble.
+	void SlowDown();//!< decreases the speed vairble.
+	void Draw(sf::RenderWindow &w);//!< draws the player.
+	sf::FloatRect GetBounds();//!< gets the players size for collision detection in other classes.
+    Pvector* getVelocity() { return velocity; } //!< return the velocity of the player.
 };
 #endif
